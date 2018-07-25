a491b5cc559b9c5bc209a26a9dd7bea6321601b0
Kerr Ivan Agapito Cirilo <kerr.ivan29@gmail.com>
2018-07-25 06:36:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ function logShout(string) {
  
  function sayHiToGrandma(string) {
     if (`${string}`.toLowerCase())
-    {return "I can't hear you!" }
+    {return "I can't hear you!"; }
     
-   else (`${string}`.toUpperCase())
+   else (`${string}`.toUpperCase());
   {return "YES INDEED!"}
 }
  
