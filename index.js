084ad622f428a833c2a3ecd868043c16bd55261d
Kerr Ivan Agapito Cirilo <kerr.ivan29@gmail.com>
2018-07-25 07:02:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,8 +21,10 @@ function logShout(string) {
  if(string.toUpperCase() === string){
    return "YES INDEED!";}
    
-   if (string === "I love u, Grandma.") { 
-     return "I love you, too."}
-   }
- 
+ if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+
+  return "Are you eating enough?"
+ }
  
